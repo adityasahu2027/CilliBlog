c7366de44a4362549e8e17abf9259a9aecff18ce
adityasahu2027 <adityasahu62599@gmail.com>
2025-09-22 20:32:52 +0530

    updated

--- Backend/jwt/authtoken.js
+++ Backend/jwt/authtoken.js
@@ -6,9 +6,9 @@ import { User } from "../model/user.js";
         expiresIn:"15d",
     });
     res.cookie("jwt",token,{
-        httpOnly:true,   
-        secure: false,  //agar localhost pe ho to false karo
-        sameSite:"strict",
+        httpOnly:false,  //client side se access karne ke liye false aur server side se access karne ke liye true 
+        secure:true,  //agar localhost pe ho to false karo nhi to true karo
+        sameSite:"none", //cross site cookie ke liye none karo
     });
     await User.findByIdAndUpdate(userId,{token});
     return token;
